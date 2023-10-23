14c66f7448a55575ad411495ef11b17167df04fa
sei <vanism7557@gmail.com>
2023-10-23 16:37:15 +0900

    Initial Commit

---
+++ BebeBoilerPlate/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BebeBoilerPlate
+//
+//  Created by sei on 10/23/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
